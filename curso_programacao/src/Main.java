4dc0b6f17f27d2c3460fda948a2f72a5beffaa76
devjoaoricardo <devjoaororamalho@gmail.com>
2025-05-08 20:45:44 -0300

    curso-java

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -6,11 +6,13 @@ public class Main {
 		int y = 32;
 		double x = 10.35784;
 		
+		
 		System.out.println(y);
 		System.out.println(x);
 		System.out.printf("%.2f%n", x);
 		System.out.println("Olá, Mundo!");
 		System.out.println("Bom dia!");
+		System.out.print(x);
 		
 	}
 
